1744e1ced37e5dcac1566eca084920b721a5988d
BlueNyang <kai@bluenyang.kr>
2026-06-19 02:24:52 +0900

    feat: add motion header and tested

---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import BannerHero from '~/components/banner-hero.vue';
+  import Header from '~/components/header.vue';
+</script>
+
+<template>
+  <div class="h-600 w-full">
+    <Header />
+    <BannerHero />
+    <h1>Index</h1>
+  </div>
+</template>
