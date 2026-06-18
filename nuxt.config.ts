316e75af98374f97940e490da611e695d668ca8b
BlueNyang <dksrbxo@bluenyang.kr>
2026-06-19 02:23:37 +0900

    feat: set font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,38 @@ export default defineNuxtConfig({
     enabled: true,
   },
 
+  app: {
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        // Google Fonts
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Jua&family=Pacifico&family=Tektur:wght@400..900&display=swap',
+        },
+        // Highlight.js CSS
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/a11y-dark.min.css',
+        },
+      ],
+      script: [
+        // Highlight.js Library
+        {
+          src: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/highlight.min.js',
+          defer: true,
+        },
+        // Inline Script 실행
+        {
+          innerHTML:
+            'document.addEventListener("DOMContentLoaded", (event) => { hljs.highlightAll(); });',
+          type: 'text/javascript',
+        },
+      ],
+    },
+  },
+
   css: ['~/assets/css/main.css'],
   site: {
     url: 'https://blog.bluenyang.kr',
