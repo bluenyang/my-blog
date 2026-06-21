3ece76d059b8f4ae19f52898959903138ce7bf3f
BlueNyang <kai@bluenyang.kr>
2026-06-22 02:05:35 +0900

    feat: rss

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+  useHead({
+    link: [
+      {
+        rel: 'alternate',
+        type: 'application/rss+xml',
+        title: 'BlueNyang Dev Blog RSS Feed',
+        href: '/rss.xml',
+      },
+    ],
+  });
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
