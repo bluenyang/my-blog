5e30704628395f4fb1c7d88b0a6f9020319666be
BlueNyang <xpsj20@gmail.com>
2026-06-21 16:43:58 +0900

    fix: data desync and style mismatch

--- app/components/category-dropdown-item.vue
+++ app/components/category-dropdown-item.vue
@@ -24,9 +24,9 @@
         </span>
       </NuxtLink>
       <div class="accordion-body grid grid-rows-[0fr] transition-all duration-300 ease-in-out">
-        <div class="border-muted-foreground min-h-0 overflow-hidden border-l pl-4">
+        <ul class="border-muted-foreground min-h-0 overflow-hidden border-l pl-4">
           <CategoryDropdownItem :items="item.children" />
-        </div>
+        </ul>
       </div>
     </div>
     <NuxtLink v-else :to="`/categories/${item.slug}`" class="flex items-center gap-2">
