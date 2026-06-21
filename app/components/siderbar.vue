024489a994dd01d05cbc43b6b64f6bd87d62dea0
BlueNyang <xpsj20@gmail.com>
2026-06-21 17:45:38 +0900

    feat: sidebar layout

---
+++ app/components/siderbar.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+  import { useSidebar } from '~/composables/use-sidebar';
+
+  const { isOpen: isSidebarOpen, close: closeSidebar } = useSidebar();
+
+  const route = useRoute();
+  watch(
+    () => route.fullPath,
+    () => {
+      if (window.innerWidth < 768) {
+        closeSidebar();
+      }
+    },
+  );
+</script>
+
+<template>
+  <div class="relative z-50 md:hidden">
+    <Transition
+      enter-active-class="transition-opacity duration-300 ease-out"
+      enter-from-class="opacity-0"
+      enter-to-class="opacity-100"
+      leave-active-class="transition-opacity duration-200 ease-in"
+      leave-from-class="opacity-100"
+      leave-to-class="opacity-0"
+    >
+      <div
+        v-if="isSidebarOpen"
+        class="fixed inset-0 bg-black/40 backdrop-blur-xs"
+        @click="closeSidebar"
+      />
+    </Transition>
+
+    <Transition
+      enter-active-class="transition-transform duration-300 ease-out"
+      enter-from-class="-translate-x-full"
+      enter-to-class="translate-x-0"
+      leave-active-class="transition-transform duration-200 ease-in"
+      leave-from-class="translate-x-0"
+      leave-to-class="-translate-x-full"
+    >
+      <div
+        v-if="isSidebarOpen"
+        class="fixed inset-y-0 left-0 flex h-full w-72 flex-col bg-white p-5 shadow-xl"
+      >
+        <button class="mb-4 self-end p-2 text-gray-500 hover:text-gray-800" @click="closeSidebar">
+          ✕
+        </button>
+      </div>
+    </Transition>
+  </div>
+
+  <aside
+    class="sticky top-0 hidden h-screen overflow-hidden border-r border-gray-200 bg-white transition-all duration-300 ease-in-out md:block"
+    :class="isSidebarOpen ? 'w-64 p-5' : 'w-0 border-r-0 p-0'"
+  >
+    <div class="flex h-full w-54 flex-col"></div>
+  </aside>
+</template>
