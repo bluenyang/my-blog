7cca011285a96d3a975e8383537ccf7fdec44aa1
BlueNyang <dksrbxo@bluenyang.kr>
2026-06-27 02:09:36 +0900

    feat: link section

--- app/components/sidebar/category-items.vue
+++ app/components/sidebar/category-items.vue
@@ -17,39 +17,48 @@
 </script>
 
 <template>
-  <li
-    class="relative w-full bg-linear-to-r from-blue-300 to-purple-400 dark:from-blue-600 dark:to-purple-700"
-  >
-    <NuxtLink
-      :to="`/categories/${item.slug}`"
-      class="bg-sidebar group relative flex w-full items-center justify-between gap-2 truncate px-2 py-1 transition-all duration-200 hover:translate-x-1"
-    >
-      <div class="flex items-center gap-2">
-        <Icon v-if="item.icon" :name="item.icon" class="size-4 text-sky-600" />
-        <span
-          class="text-base font-medium group-hover:text-purple-700 group-hover:dark:text-indigo-300"
-        >
-          {{ item.name }}
-        </span>
-        <span class="bg-muted-foreground rounded-full px-2 text-xs font-semibold">
-          {{ item.postCount || 0 }}
-        </span>
-      </div>
-      <button
-        v-if="hasChildren"
-        class="flex cursor-pointer focus:outline-none"
-        @click="toggleChildOpen"
-      >
-        <Icon :name="isChildOpen ? 'lucide:chevron-down' : 'lucide:chevron-right'" />
-      </button>
-    </NuxtLink>
+  <li class="relative w-full">
     <div
-      v-if="item.children && item.children.length > 0"
-      :class="cn(isChildOpen ? 'block' : 'hidden')"
+      class="w-full bg-linear-to-r from-blue-300 to-purple-400 dark:from-blue-600 dark:to-purple-700"
     >
-      <ul class="bg-sidebar ps-3">
-        <CategoryItems v-for="child in item.children" :key="child.id" :item="child" />
-      </ul>
+      <NuxtLink
+        :to="`/categories/${item.slug}`"
+        class="bg-sidebar group relative flex w-full items-center justify-between gap-2 truncate px-2 py-1 transition-all duration-200 hover:translate-x-1"
+      >
+        <div class="flex items-center gap-2">
+          <Icon v-if="item.icon" :name="item.icon" class="size-4 text-sky-600" />
+          <span
+            class="text-base font-medium group-hover:text-purple-700 group-hover:dark:text-indigo-300"
+          >
+            {{ item.name }}
+          </span>
+          <span class="bg-muted-foreground rounded-full px-2 text-xs font-semibold">
+            {{ item.postCount || 0 }}
+          </span>
+        </div>
+        <button
+          v-if="hasChildren"
+          class="flex cursor-pointer focus:outline-none"
+          @click="toggleChildOpen"
+        >
+          <Icon
+            name="radix-icons:caret-down"
+            class="size-4 transition-transform duration-200"
+            :class="isChildOpen ? 'rotate-180' : ''"
+          />
+        </button>
+      </NuxtLink>
+    </div>
+    <div
+      v-if="hasChildren"
+      class="grid overflow-hidden transition-[grid-template-rows] duration-200 ease-in-out"
+      :class="isChildOpen ? 'grid-rows-[1fr]' : 'grid-rows-[0fr]'"
+    >
+      <div class="overflow-hidden">
+        <ul class="bg-sidebar ps-3">
+          <CategoryItems v-for="child in item.children" :key="child.id" :item="child" />
+        </ul>
+      </div>
     </div>
   </li>
 </template>
