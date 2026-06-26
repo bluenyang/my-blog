eeac598b1eaebdcb658e14dcf49cc7e0836140eb
BlueNyang <dksrbxo@bluenyang.kr>
2026-06-27 02:39:26 +0900

    feat: add hover indicator for sidebar items

--- app/components/sidebar/categories.vue
+++ app/components/sidebar/categories.vue
@@ -8,15 +8,16 @@
   <div class="px-2 py-4">
     <ul class="flex flex-col gap-2">
       <li
-        class="relative w-full bg-linear-to-r from-blue-300 to-purple-400 dark:from-blue-600 dark:to-purple-700"
+        class="relative w-full md:bg-linear-to-b md:from-blue-300 md:to-purple-400 dark:md:from-blue-400 dark:md:to-purple-500"
       >
         <NuxtLink
           :to="`/posts`"
-          class="bg-sidebar group relative flex w-full items-center gap-2 truncate px-2 py-1 transition-all duration-200 hover:translate-x-1"
+          class="bg-sidebar group relative flex w-full items-center gap-2 truncate px-2 py-1 transition-all duration-200 md:hover:translate-x-1"
         >
-          <span class="font-medium group-hover:text-purple-700 group-hover:dark:text-indigo-300">{{
-            '전체 글'
-          }}</span>
+          <span
+            class="font-medium md:group-hover:text-purple-700 md:group-hover:dark:text-indigo-300"
+            >{{ '전체 글' }}</span
+          >
           <span class="bg-muted-foreground rounded-full px-3 text-sm">
             {{ total || 0 }}
           </span>
--- app/components/sidebar/link-items.vue
+++ app/components/sidebar/link-items.vue
@@ -23,25 +23,28 @@
       :to="item.url ?? ''"
       :target="item.url ? '_blank' : undefined"
       rel="noopener noreferrer"
-      class="flex items-center justify-between rounded-lg px-2 py-1.5"
-      :class="cn(!hasChild && 'hover:bg-accent hover:text-accent-foreground cursor-pointer')"
+      class="flex items-center justify-between rounded-lg"
+      :class="cn(!hasChild && 'md:hover:bg-accent md:hover:text-accent-foreground cursor-pointer')"
     >
-      <div class="flex items-center gap-2">
-        <Icon :name="item.icon || 'lucide:link'" class="size-4" />
-        <span>{{ item.label }}</span>
+      <div class="flex min-w-0 items-center gap-2 px-2 py-1.5">
+        <Icon :name="item.icon || 'lucide:link'" class="size-4 shrink-0" />
+        <span class="truncate">{{ item.label }}</span>
       </div>
-      <button @click="toggleChild">
+      <button
+        v-if="hasChild"
+        class="md:hover:bg-sidebar-accent me-1 flex size-6 shrink-0 items-center justify-center rounded-full"
+        @click="toggleChild"
+      >
         <Icon
-          v-if="hasChild"
           name="radix-icons:caret-down"
-          class="size-4 transition-transform duration-200"
+          class="size-6 transition-transform duration-200"
           :class="childOpen ? 'rotate-180' : ''"
         />
       </button>
     </NuxtLink>
     <ul
       v-if="hasChild"
-      class="ml-2 overflow-hidden transition-all duration-200 ease-in-out"
+      class="ml-3 overflow-hidden transition-all duration-200 ease-in-out"
       :class="cn(childOpen ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0')"
     >
       <LinkItems v-for="child in item.children" :key="child.id" :item="child" />
