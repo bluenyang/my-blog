a252ed1825152779ee56547d3a50b60650f29d47
BlueNyang <dksrbxo@bluenyang.kr>
2026-07-01 18:57:56 +0900

    refactor: twmerge

--- app/components/sidebar/categories.vue
+++ app/components/sidebar/categories.vue
@@ -13,8 +13,12 @@
         class="relative w-full md:bg-linear-to-b md:from-blue-300 md:to-purple-400 dark:md:from-blue-400 dark:md:to-purple-500"
       >
         <div
-          class="bg-sidebar group relative flex w-full items-center gap-2 truncate transition-all duration-200 md:hover:translate-x-1"
-          :class="route.path === '/posts' ? 'bg-sidebar-accent text-sidebar-accent-foreground' : ''"
+          :class="
+            cn(
+              'bg-sidebar group flex items-center justify-between gap-2 transition-all duration-200 md:hover:translate-x-1',
+              route.path === '/posts' && 'bg-sidebar-accent text-sidebar-accent-foreground',
+            )
+          "
         >
           <NuxtLink :to="`/posts`" class="flex min-w-0 flex-1 items-center gap-2 px-2 py-1">
             <Icon
@@ -24,21 +28,23 @@
             />
             <Icon v-else name="radix-icons:archive" class="text-sidebar-primary size-4 shrink-0" />
             <span
-              class="text-lg font-medium transition-colors"
               :class="
-                route.path === '/posts'
-                  ? 'text-sidebar-primary font-bold'
-                  : 'md:group-hover:text-purple-700 md:group-hover:dark:text-indigo-300'
+                cn(
+                  'text-lg font-medium transition-colors',
+                  route.path === '/posts'
+                    ? 'text-sidebar-primary font-bold'
+                    : 'group-hover:text-purple-700 dark:group-hover:text-indigo-300',
+                )
               "
             >
               {{ '전체 글' }}
             </span>
             <span
-              class="bg-muted-foreground rounded-full px-3 text-sm font-semibold"
               :class="
-                route.path === '/posts'
-                  ? 'text-sidebar-primary-foreground bg-sidebar-primary'
-                  : 'text-white'
+                cn(
+                  'bg-sidebar-accent-hover rounded-full px-3 text-sm font-semibold',
+                  route.path === '/posts' && 'text-sidebar-primary-foreground bg-sidebar-primary',
+                )
               "
             >
               {{ total || 0 }}
--- app/components/sidebar/category-items.vue
+++ app/components/sidebar/category-items.vue
@@ -48,8 +48,12 @@
       class="md:bg-linear-to-b md:from-blue-300 md:to-purple-400 dark:md:from-blue-400 dark:md:to-purple-500"
     >
       <div
-        class="bg-sidebar group flex items-center justify-between gap-2 transition-all duration-200 md:hover:translate-x-1"
-        :class="isActive ? 'bg-sidebar-accent text-sidebar-accent-foreground' : ''"
+        :class="
+          cn(
+            'bg-sidebar group flex items-center justify-between gap-2 transition-all duration-200 md:hover:translate-x-1',
+            isActive && 'bg-sidebar-accent text-sidebar-accent-foreground',
+          )
+        "
       >
         <NuxtLink
           :to="{ path: '/search', query: { category: item.slug } }"
@@ -57,18 +61,24 @@
         >
           <Icon v-if="item.icon" :name="item.icon" class="size-4 shrink-0 text-sky-600" />
           <span
-            class="flex-1 truncate text-base font-medium transition-colors"
             :class="
-              isActive
-                ? 'text-sidebar-primary font-bold'
-                : 'md:group-hover:text-purple-700 md:group-hover:dark:text-indigo-300'
+              cn(
+                'flex-1 truncate text-base font-medium transition-colors',
+                isActive
+                  ? 'text-sidebar-primary font-bold'
+                  : 'md:group-hover:text-purple-700 md:group-hover:dark:text-indigo-300',
+              )
             "
           >
             {{ item.name }}
           </span>
           <span
-            class="bg-muted-foreground shrink-0 rounded-full px-2 text-xs font-semibold"
-            :class="isActive ? 'text-sidebar-primary-foreground bg-sidebar-primary' : 'text-white'"
+            :class="
+              cn(
+                'bg-sidebar-accent-hover rounded-full px-3 text-sm font-semibold',
+                isActive && 'text-sidebar-primary-foreground bg-sidebar-primary',
+              )
+            "
           >
             {{ item.postCount || 0 }}
           </span>
@@ -82,16 +92,19 @@
         >
           <Icon
             name="radix-icons:caret-down"
-            class="size-6 transition-transform duration-200"
-            :class="isChildOpen ? 'rotate-180' : ''"
+            :class="cn('size-6 transition-transform duration-200', isChildOpen && 'rotate-180')"
           />
         </button>
       </div>
     </div>
     <div
       v-if="hasChildren"
-      class="grid transition-[grid-template-rows] duration-200 ease-in-out"
-      :class="isChildOpen ? 'grid-rows-[1fr]' : 'grid-rows-[0fr]'"
+      :class="
+        cn(
+          'grid grid-rows-[0fr] transition-[grid-template-rows] duration-200 ease-in-out',
+          isChildOpen && 'grid-rows-[1fr]',
+        )
+      "
     >
       <div class="overflow-hidden">
         <ul class="bg-sidebar ps-3">
