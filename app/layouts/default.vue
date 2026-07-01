4205d135e905d5c60d0de62077d14a78b30315ea
BlueNyang <dksrbxo@bluenyang.kr>
2026-07-02 00:37:26 +0900

    feat: add footer

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex h-600 flex-row">
+  <div class="flex flex-row">
     <Siderbar />
 
     <div class="relative flex grow flex-col">
@@ -9,6 +9,7 @@
       <main class="flex-1">
         <slot />
       </main>
+      <Footer />
     </div>
   </div>
 </template>
