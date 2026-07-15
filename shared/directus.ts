60489dfe5b25e8e489b6b4437e2cb750f3eb9786
BlueNyang <dksrbxo@bluenyang.kr>
2026-07-16 01:38:39 +0900

    feat: add directus plugin

---
+++ shared/directus.ts
@@ -0,0 +1,8 @@
+import { createDirectus, rest } from '@directus/sdk';
+
+/** app plugin / server utils에서 공유하는 Directus REST 클라이언트 팩토리 */
+export function createDirectusClient(url: string) {
+  return createDirectus(url).with(rest());
+}
+
+export type DirectusClient = ReturnType<typeof createDirectusClient>;
