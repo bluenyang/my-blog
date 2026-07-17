286abbf1cc1737602b6b1d62165595a6bb68f602
BlueNyang <xpsj20@gmail.com>
2026-07-18 03:36:43 +0900

    feat: post detail fetch

---
+++ server/api/post/[idx].get.ts
@@ -0,0 +1,43 @@
+import { useQuery } from '#server/utils/use-query';
+import { postDetailMapper, sidebarMapper } from '~~/server/features/mapper';
+import type { PostDetail } from '~~/shared/types/post';
+import type { RawSidebarContent } from '~~/shared/types/sidebar';
+
+export default defineEventHandler(async (event): Promise<PostDetail> => {
+  const postIdx = Number(getRouterParam(event, 'idx'));
+  const needSidebar = getQuery(event).sidebar === 'true';
+
+  const directus = useDirectus();
+  const { buildQuery, sidebar, postDetail } = useQuery();
+
+  if (isNaN(postIdx)) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Invalid post index',
+    });
+  }
+
+  try {
+    const result = await directus.query<RawPostDetail & Partial<RawSidebarContent>>(
+      buildQuery(postDetail(postIdx), needSidebar ? sidebar : undefined),
+    );
+
+    // return result;
+
+    const postDetailData = postDetailMapper(result);
+    const sidebarDetail = needSidebar ? sidebarMapper(result) : undefined;
+
+    const payload = postDetailData;
+    if (needSidebar) {
+      payload.sidebar = sidebarDetail;
+    }
+
+    return payload;
+  } catch (error) {
+    console.error('Failed to fetch post detail:', error);
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Failed to fetch post detail',
+    });
+  }
+});
