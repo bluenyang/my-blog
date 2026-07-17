0bdfafc2e85c15b0e709e112c1956f93b245cf2b
BlueNyang <kai@bluenyang.kr>
2026-07-18 01:37:00 +0900

    feat: set graphql query

---
+++ server/features/sidebar.query.ts
@@ -0,0 +1,57 @@
+export function sidebarQuery(blogSlug: string) {
+  return `sidebarPostCount: posts_aggregated(
+    filter: {
+      blog_id: { slug: { _eq: "${blogSlug}" } }
+      status: { _eq: "published" }
+    }
+  ) {
+    count { id }
+  }
+  sidebarCategories: categories(
+    sort: ["sort_order", "slug"]
+    filter: {
+      blog_id: { slug: { _eq: "${blogSlug}" } }
+    }
+  ) {
+    id
+    parent_id { id }
+    name
+    icon
+    slug
+    posts_func { count }
+  }
+  sidebarSeries: series(
+    filter: {
+      blog_id: { slug: { _eq: "${blogSlug}" } }
+    }
+  ) {
+    id
+    name
+    slug
+    description
+    thumbnail { id }
+    posts_func { count }
+  }
+  sidebarTags: tags(
+    filter: {
+      blog_id: { slug: { _eq: "${blogSlug}" } }
+    }
+  ) {
+    id
+    name
+    slug
+    posts_func { count }
+  }
+  sidebarNavigations: navigations(
+    filter: {
+      blog_id: { slug: { _eq: "${blogSlug}" } }
+    }
+  ) {
+    id
+    label
+    url
+    icon
+    is_category
+    parent_id { id }
+  }`;
+}
