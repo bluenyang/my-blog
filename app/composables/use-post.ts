d5f8695855e2cc8038b1546383b67ee8f4fc2e21
BlueNyang <kai@bluenyang.kr>
2026-07-19 05:53:59 +0900

    refactor: use ssr cached data

--- app/composables/use-post.ts
+++ app/composables/use-post.ts
@@ -2,6 +2,11 @@ export function usePostDetail(postIdx: number | string) {
   const { data, pending, error } = useFetch<PostDetail>(`/api/post/${postIdx}`, {
     method: 'GET',
     key: `post-${postIdx}`,
+    getCachedData(key, nuxtApp) {
+      if (nuxtApp.isHydrating) {
+        return nuxtApp.payload.data[key] ?? nuxtApp.static.data[key];
+      }
+    },
   });
 
   return {
