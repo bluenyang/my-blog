80217f205ed477f7162c786ace064a819085773b
BlueNyang <kai@bluenyang.kr>
2026-07-19 03:30:41 +0900

    refactor: use comark

--- app/utils/parse-content.ts
+++ app/utils/parse-content.ts
@@ -1,14 +1,20 @@
-import type { MDCParserResult } from '@nuxtjs/mdc';
+// import type { MDCParserResult } from '@nuxtjs/mdc';
 
-export async function parseContent(content: string): Promise<MDCParserResult> {
-  const ast = await parseMarkdown(content, {
-    rehype: {
-      plugins: {
-        unwrapImages: {
-          instance: rehypeUnwrapImages,
-        },
-      },
-    },
+import { parse } from '@comark/nuxt/parse';
+import toc from 'comark/plugins/toc';
+import highlight from '@comark/nuxt/plugins/highlight';
+import oneDarkPro from '@shikijs/themes/one-dark-pro';
+import type { ComarkTree } from 'comark';
+
+export async function parseContent(content: string): Promise<ComarkTree> {
+  return parse(content, {
+    plugins: [toc(), highlight({ themes: { light: oneDarkPro, dark: oneDarkPro } })],
+    // rehype: {
+    //   plugins: {
+    //     unwrapImages: {
+    //       instance: rehypeUnwrapImages,
+    //     },
+    //   },
+    // },
   });
-  return ast;
 }
