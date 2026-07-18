b22831620763f8e0beb9e3b6838bdf3d11d2341e
BlueNyang <kai@bluenyang.kr>
2026-07-18 23:00:58 +0900

    feat: add metadata in post list fetch

--- server/features/query.ts
+++ server/features/query.ts
@@ -1,3 +1,6 @@
+export * from './category.query';
 export * from './home.query';
 export * from './post.query';
+export * from './series.query';
 export * from './sidebar.query';
+export * from './tag.query';
